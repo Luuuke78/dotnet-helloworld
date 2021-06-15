f0f60ec621ad634dbe5023a16a956442aaeabbed
cbrachmann <christoph.brachmann@mevis.fraunhofer.de>
2021-06-15 09:55:56 +0200

    add CalovoParser

---
+++ CalovoParser/CalovoParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parser.Services
+{
+    public class Event
+    {
+        public string datetime;
+        public string summary;
+
+        public Event() {
+            this.datetime = "";
+            this.summary = "";
+        }
+    }
+    public class CalovoParser
+    {
+        public Event GetNextEvent()
+        {
+            Event g = new Event();
+            return g;
+        }
+    }
+}
