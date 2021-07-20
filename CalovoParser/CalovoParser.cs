93a4569d9f45ed5900643e0c56df1445244bd361
cbrachmann <christoph.brachmann@mevis.fraunhofer.de>
2021-07-20 10:42:05 +0200

    start with TDD

--- CalovoParser/CalovoParser.cs
+++ CalovoParser/CalovoParser.cs
@@ -1,4 +1,7 @@
-﻿public class Event
+﻿
+using System;
+
+public class Event
 {
     public string datetime;
     public string summary;
@@ -10,9 +13,33 @@
 }
 public class CalovoParser
 {
-    public Event GetNextEvent()
+    private string calendar;
+    public CalovoParser(string calendar) {
+        Console.WriteLine(calendar);
+        this.calendar = calendar;
+    }
+    public Event GetNextEvent(string startDate)
     {
+        string text, datum, spiel, zeile;
+        int a, b, c, d, e;
         Event g = new Event();
+
+        b = this.calendar.IndexOf("DTSTART");
+        if (b > -1) {
+            c = this.calendar.IndexOf("SEQUENCE");
+            zeile = this.calendar.Substring(b, c - b);
+            d = zeile.IndexOf("#");
+            g.datetime = zeile.Substring(d + 1, 15);
+        }
+
+        b = this.calendar.IndexOf("SUMMARY");
+        if (b > -1) {
+            c = this.calendar.IndexOf("DESCRIPTION");
+            zeile = this.calendar.Substring(b, c - b);
+            d = zeile.IndexOf("#");
+            e = zeile.IndexOf("|");
+            g.summary = zeile.Substring(d + 1, e - d - 1).Trim();
+        }
         return g;
     }
 }
--- CalovoParserTestMS/UnitTest1.cs
+++ CalovoParserTestMS/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CalovoParserTestMS
 {
@@ -6,8 +7,44 @@ namespace CalovoParserTestMS
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_GetNextEvent_emptyCal_returnsEmptySummaryAndDate()
         {
+            CalovoParser p = new CalovoParser("");
+            Assert.AreEqual("", p.GetNextEvent("").summary);
         }
+
+        [TestMethod]
+        public void Test_GetNextEvent_calWithOneEntry_returnsReferenceEvent()
+        {
+            string calendar = @"----DTSTART;TZID=Europe/Berlin:20210721T183000
+SEQUENCE:0
+DTEND;TZID=Europe/Berlin:20200606T203000
+SUMMARY:Borussia Dortmund - Hertha BSC | Bundesliga | 30. Spieltag
+DESCRIPTION:----";
+            calendar = calendar.Replace(":", "#");
+            CalovoParser p = new CalovoParser(calendar);
+            Event e = p.GetNextEvent("20210720");
+            Assert.AreEqual("Borussia Dortmund - Hertha BSC", e.summary);
+            Assert.AreEqual("20210721T183000", e.datetime);
+        }
+
+        [TestMethod]
+        public void Test_GetNextEvent_calWithOlderEntry_returnsEmptyEvent()
+        {
+            string calendar = @"----DTSTART;TZID=Europe/Berlin:20210721T183000
+SEQUENCE:0
+DTEND;TZID=Europe/Berlin:20200606T203000
+SUMMARY:Borussia Dortmund - Hertha BSC | Bundesliga | 30. Spieltag
+DESCRIPTION:----";
+            calendar = calendar.Replace(":", "#");
+            CalovoParser p = new CalovoParser(calendar);
+            Event e = p.GetNextEvent("20210812");
+            
+            // TODO lbra fix this test by implementing GetNextEvent
+            Assert.AreEqual("", e.summary);
+            Assert.AreEqual("", e.datetime);
+        }
+
+
     }
 }
