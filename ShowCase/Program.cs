04413c38751265649dea47843aeb48f61f5297dd
cbrachmann <christoph.brachmann@mevis.fraunhofer.de>
2021-09-02 00:07:12 +0200

    add showcase

---
+++ ShowCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ParserLibs;
+
+
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        string text = System.IO.File.ReadAllText(@"assets\buli.ics");
+        CalovoParser cp = new CalovoParser(text);
+        List<Event> events = cp.GetAllNextEvents("20210827");
+        Console.WriteLine(events[0].datetime);
+    }
+}
